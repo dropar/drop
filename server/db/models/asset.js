090e79e91b24e052b87b5558b902fba7cb8a6c00
johnlatona <johnbl24@aol.com>
2018-10-01 12:50:46 -0400

    add assets model, create associations between asset and user, force true db sync in development only

--- server/db/models/asset.js
+++ server/db/models/asset.js
@@ -1,16 +1,12 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
 
-const Product = db.define('product', {
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
+const Asset = db.define('asset', {
   assetUrl: {
     type: Sequelize.TEXT,
     allowNull: false
   }
 })
 
-module.exports = Product;
+module.exports = Asset;
 
