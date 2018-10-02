f6661c6b01f9137a8199110346d1712790c07a3d
Ryan Johnson <ryanpjohnson14@gmail.com>
2018-10-02 14:41:29 -0400

    built user assets html and js files

---
+++ public/userAssets.js
@@ -0,0 +1,16 @@
+const axios = require('axios')
+
+
+const assetsDiv = document.getElementById('user-assets');
+const userId = this.user.id;
+let userAssets;
+assetsDiv.addEventListener('load', () => {
+  userAssets = axios.get(`/users/${userId}/assets`)
+  return userAssets;
+})
+
+userAssets.forEach((asset) => {
+  const userAssetNode = document.createElement('p')
+  userAssetNode.innerText = asset.name;
+  assetsDiv.appendChild(userAssetNode)
+})
